ee339f72ccecc3808dbfc75168f79f3a5ab5f129
dattatray001 <114179112+dattatray001@users.noreply.github.com>
2023-09-10 17:10:05 +0530

    1 file modified, 3 added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   return (
     <div className="bg-darj">
       <h1 className="text-success">hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
